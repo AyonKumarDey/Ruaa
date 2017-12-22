ded4b4b6b0a83e48e3b842848f7baa71bbee35b3
Soham <soham.das78@gmail.com>
2017-12-22 23:17:44 +0530

    Git Update performed by Soham Das
    Modification Performed :-
    1. Gradle Dependencies Updated (Volley Library and others) and AWS IP class established.

--- app/src/main/java/com/example/ayonkumardey/ruaa/IPResolver.java
+++ app/src/main/java/com/example/ayonkumardey/ruaa/IPResolver.java
@@ -1,8 +1,8 @@
 package com.example.ayonkumardey.ruaa;
 public class IPResolver
 {
-    //public static String ip="http://www.bookdecr.com/";
-    public static String ip="http://8.8.8.8/";
+    //public static String ip="http://www.ruaa.in/";
+    public static String ip="http://8.8.8.8/";//Update AWS IP here....commit required
     public static String getIp() {
         return ip;
     }
